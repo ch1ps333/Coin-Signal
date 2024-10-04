ee5470b7d512329ae8e7eaafe1b216c03c5b5e5d
ch1ps333 <95697368+ch1ps333@users.noreply.github.com>
2024-10-05 01:47:01 +0300

    Update bot_commands.py

--- handlers/bot_commands.py
+++ handlers/bot_commands.py
@@ -80,17 +80,23 @@ async def send_notification(ukr_text, en_text, prev_percent_change, now_percent_
         users = await get_all_users()
         if users != []:
             for user in users:
-                if minute_analysis in user.signal_interval and user and ((now_percent_change < 0 and now_percent_change <= user.degreas_percent and abs(prev_percent_change - now_percent_change) >= user.degreas_percent) or (now_percent_change > 0 and now_percent_change >= user.increas_percent and abs(prev_percent_change - now_percent_change) >= user.increas_percent)):
+                if minute_analysis in user.signal_interval and user and ((now_percent_change < 0 and now_percent_change <= user.degreas_percent and abs(prev_percent_change - now_percent_change) >= abs(user.degreas_percent)) or (now_percent_change > 0 and now_percent_change >= user.increas_percent and abs(prev_percent_change - now_percent_change) >= user.increas_percent)):
                     try:
                         if now_percent_change > 200 and abs(prev_percent_change - now_percent_change) < 50:
                             return False
                         keyboard = await display_coin_spot(link, user.tg_id)
                         if user.lang == 'ukr':
                             await send_message(user.tg_id, ukr_text, session, keyboard)
-                            await add_signal_history(ukr_text, user.tg_id)
+                            try:
+                                await add_signal_history(ukr_text, user.tg_id)
+                            except Exception as err:
+                                print(err)
                         else:
                             await send_message(user.tg_id, en_text, session, keyboard)
-                            await add_signal_history(en_text, user.tg_id)
+                            try:
+                                await add_signal_history(en_text, user.tg_id)
+                            except Exception as err:
+                                print(err)
                         await sleep(0.5)
                     except Exception as err:
                         print(f"{user.tg_id}: {err}")
